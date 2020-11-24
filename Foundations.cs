07223ccca3440f46eef6f2a8b7e110f7b7f9170a
MooseTheGoose <moosa.7@buckeyemail.osu.edu>
2020-11-23 20:06:10 -0500

    make visual studio project

---
+++ Foundations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Foundations
+{
+    class Foundations
+    {
+        public static void Main(string[] args)
+        {
+            using (StreamReader rdr = File.OpenText("..\\..\\Foundations.cs"))
+            {
+                string fcontents = rdr.ReadToEnd();
+                Console.WriteLine("Printing Myself");
+                Console.WriteLine("---------------");
+                Console.WriteLine(fcontents);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Lexer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Foundations
+{
+    enum TokenType 
+    {
+        TERM = 0
+    }
+
+    enum TokenOperatorType 
+    {
+    }
+
+    struct Token 
+    {
+        public TokenType type;
+        public int lino;
+        public int chno;
+        public object data;
+    }
+
+    class Lexer
+    {
+        public int curr_lino;
+        public int curr_chno;
+
+        public Lexer()
+        {
+            this.curr_lino = 1;
+            this.curr_chno = 1;
+        }
+    }
+}
